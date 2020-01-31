aae3a934d8f7f6fc08f4a83b9e8b88b93e42745a
ketil-A <ketil@unicus.no>
2020-01-31 13:56:44 +0100

    Renamed service

--- Models/ServiceType.cs
+++ Models/ServiceType.cs
@@ -5,9 +5,9 @@ using System.Threading.Tasks;
 
 namespace MovingAPI.Models
 {
-    public class Service
+    public class ServiceType
     {
-        public int ServiceID { get; set; }
+        public int ServiceTypeID { get; set; }
         public string Name { get; set; }
     }
 }
