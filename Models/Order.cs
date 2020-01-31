6594813a57a00f91fd19791378d71b44d90c69c5
Knut-H <Knut@unicus.no>
2020-01-31 13:56:30 +0100

    Created and populated Order

---
+++ Models/Order.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovingAPI.Models
+{
+    public class Order
+    {
+        public int OrderID { get; set; }
+        public string FromAddress { get; set; }
+        public string ToAddress { get; set; }
+        public string Notes { get; set; }
+        public DateTime DateOfService { get; set; }
+
+        //Foreign keys and data structures
+        public Customer Customer { get; set; }
+        public int CustomerID { get; set; }
+
+        public ICollection<OrderServiceType> OrderServices { get; set; }
+    }
+}
